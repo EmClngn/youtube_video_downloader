d185c93a989727d29ff69a832dc19541cf485272
unknown <emgcalangian@gmail.com>
2024-02-03 10:07:15 +0800

    pseudo code

---
+++ youtube_video_downloader.py
@@ -0,0 +1,8 @@
+# The vision is to create a youtube video downloader that is able to download videos and audio only
+
+# pseudo code 
+
+# follow the youtube video: https://youtu.be/vEQ8CXFWLZU?t=325
+# try to add a way to change video format into mp3 for music downloading
+# error handling
+# after all of that is done, make everything work with tkinter gui
