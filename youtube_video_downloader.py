37d3fdef7390c6ed7ee8f187372ac91fbd12ff0b
unknown <emgcalangian@gmail.com>
2024-02-03 21:05:12 +0800

    added user inputs for url and download path

--- youtube_video_downloader.py
+++ youtube_video_downloader.py
@@ -1,22 +1,23 @@
 # The vision is to create a youtube video downloader that is able to download videos and audio only
 from pytube import YouTube
-import subprocess
 
 # pseudo code 
 
 # follow the youtube video: https://youtu.be/vEQ8CXFWLZU?t=325
 # adjust some things with the code in the yt video once have more knowledge about pytube
-link = "https://youtu.be/jHjUHKdoaqI"
-yt = YouTube(link)
+user_link = input("Paste here your desired Youtube link: ")
+yt = YouTube(user_link)
+download_path = input("Paste here your desired download path: ")
 
 print("Title: ", yt.title)
 print("View: ", yt.views)
 
 # idea is to combine the video with the downloaded audio but can't get ffmpeg to work
 video = yt.streams.get_highest_resolution()
-
+video.download(output_path=download_path)
+print("Video Download Complete")
 # download video instead but for audio only
 audio = yt.streams.filter(only_audio=True, mime_type = 'audio/mp4').first()
-audio.download(r'd:\youtube download')
-
+audio.download(output_path=download_path)
+print("Audio Download Complete")
 # after all of that is done, make everything work with tkinter gui
