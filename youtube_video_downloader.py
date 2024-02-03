25c1ad73ebbb444bd52c5d416d0e9891c4d05396
unknown <emgcalangian@gmail.com>
2024-02-03 11:17:10 +0800

    resolutions caps to 720p so did some fix to get the highest resolution but now it has no audio

--- youtube_video_downloader.py
+++ youtube_video_downloader.py
@@ -4,16 +4,17 @@ from pytube import YouTube
 # pseudo code 
 
 # follow the youtube video: https://youtu.be/vEQ8CXFWLZU?t=325
-link = "https://youtu.be/i9SwzEhfBLc?si=O3lrZczmzTmQSEL7"
+# adjust some things with the code in the yt video once have more knowledge about pytube
+link = "https://youtu.be/jHjUHKdoaqI"
 yt = YouTube(link)
 
 print("Title: ", yt.title)
 print("View: ", yt.views)
 
-yd = yt.streams.get_highest_resolution()
+yd = yt.streams.filter(adaptive=True).filter(mime_type='video/webm').first()
 yd.download('d:\youtube download')
 
-# adjust some things with the code in the yt video once have more knowledge about pytube
+
 # try to add a way to change video format into mp3 for music downloading
 # error handling
 # after all of that is done, make everything work with tkinter gui
