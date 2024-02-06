eded921882e0c4cabbede24616faf59675b38f2c
unknown <emgcalangian@gmail.com>
2024-02-06 15:48:51 +0800

    more adjustments and error handling

--- youtube_video_downloader.py
+++ youtube_video_downloader.py
@@ -1,32 +1,42 @@
 # The vision is to create a youtube video downloader that is able to download videos and audio only
 from pytube import YouTube
 
-
 # pseudo code 
 
 # follow the youtube video: https://youtu.be/vEQ8CXFWLZU?t=325
 # adjust some things with the code in the yt video once have more knowledge about pytube
 # idea is to combine the video with the downloaded audio but can't get ffmpeg to work
 def video_downlaoder():
-    video = yt.streams.get_highest_resolution()
-    video.download(output_path=download_path)
-    print("Video Download Complete")
+    try:
+        user_link = input("Paste here your desired Youtube link: ")
+        download_path = input("Paste here your desired download path: ")
+        yt = YouTube(user_link)
+        print("Title: ", yt.title)
+        print("View: ", yt.views)
+        video = yt.streams.get_highest_resolution()
+        video.download(output_path=download_path)
+        print("Video Download Complete")
+    except Exception as e:
+        print("An error occured:", str(e))
 # download video instead but for audio only
 def audio_downlaoder():
-    audio = yt.streams.filter(only_audio=True, mime_type = 'audio/mp4').first()
-    audio.download(output_path=download_path)
-    print("Audio Download Complete")
+    try:
+        user_link = input("Paste here your desired Youtube link: ")
+        download_path = input("Paste here your desired download path: ")
+        yt = YouTube(user_link)
+        print("Title: ", yt.title)
+        print("View: ", yt.views)
+        audio = yt.streams.filter(only_audio=True, mime_type = 'audio/mp4').first()
+        audio.download(output_path=download_path)
+        print("Audio Download Complete")
+    except Exception as e:
+        print("An error occured:", str(e))
 
 
-user_link = input("Paste here your desired Youtube link: ")
-download_path = input("Paste here your desired download path: ")
+
 options = input("Press '0' if you want Video or press '1' if you want Audio: ")
 option_1 = "0"
 option_2 = "1"
-
-yt = YouTube(user_link)
-print("Title: ", yt.title)
-print("View: ", yt.views)
 if options == option_1:
     video_downlaoder()
 elif options == option_2:
