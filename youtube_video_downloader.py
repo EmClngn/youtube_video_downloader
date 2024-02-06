f4da1115b1d5e6ffdd5bdf83e24797357775ea4a
unknown <emgcalangian@gmail.com>
2024-02-06 16:00:49 +0800

    added while loop

--- youtube_video_downloader.py
+++ youtube_video_downloader.py
@@ -4,33 +4,43 @@ from pytube import YouTube
 # pseudo code 
 
 # follow the youtube video: https://youtu.be/vEQ8CXFWLZU?t=325
+
 # adjust some things with the code in the yt video once have more knowledge about pytube
 # idea is to combine the video with the downloaded audio but can't get ffmpeg to work
+
+# downloading the whole video
 def video_downlaoder():
-    try:
-        user_link = input("Paste here your desired Youtube link: ")
-        download_path = input("Paste here your desired download path: ")
-        yt = YouTube(user_link)
-        print("Title: ", yt.title)
-        print("View: ", yt.views)
-        video = yt.streams.get_highest_resolution()
-        video.download(output_path=download_path)
-        print("Video Download Complete")
-    except Exception as e:
-        print("An error occured:", str(e))
+    while True:
+        try:
+            user_link = input("Paste here your desired Youtube link: ")
+            download_path = input("Paste here your desired download path: ")
+            yt = YouTube(user_link)
+            print("Title: ", yt.title)
+            print("View: ", yt.views)
+            video = yt.streams.get_highest_resolution()
+            video.download(output_path=download_path)
+            print("Video Download Complete")
+            break
+        except Exception as e:
+            print("An error occured:", str(e))
+            print("Please input a valid YouTube URL or valid download path. Please try again.")
+
 # download video instead but for audio only
 def audio_downlaoder():
-    try:
-        user_link = input("Paste here your desired Youtube link: ")
-        download_path = input("Paste here your desired download path: ")
-        yt = YouTube(user_link)
-        print("Title: ", yt.title)
-        print("View: ", yt.views)
-        audio = yt.streams.filter(only_audio=True, mime_type = 'audio/mp4').first()
-        audio.download(output_path=download_path)
-        print("Audio Download Complete")
-    except Exception as e:
-        print("An error occured:", str(e))
+    while True:
+        try:
+            user_link = input("Paste here your desired Youtube link: ")
+            download_path = input("Paste here your desired download path: ")
+            yt = YouTube(user_link)
+            print("Title: ", yt.title)
+            print("View: ", yt.views)
+            audio = yt.streams.filter(only_audio=True, mime_type = 'audio/mp4').first()
+            audio.download(output_path=download_path)
+            print("Audio Download Complete")
+            break
+        except Exception as e:
+            print("An error occured:", str(e))
+            print("Please input a valid YouTube URL or valid download path. Please try again.")
 
 
 
